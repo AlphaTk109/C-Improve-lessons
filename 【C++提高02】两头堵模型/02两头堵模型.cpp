85dba7823236362ed0c1ab4be6cee48d3fd6dd4c
子健 刘 <alphatk@vip.qq.com>
2020-07-14 22:42:09 +0800

    两头堵模型 （API去除字符串两边空格实例）

--- 【C++提高02】两头堵模型/02两头堵模型.cpp
+++ 【C++提高02】两头堵模型/02两头堵模型.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+#pragma warning (disable:4996)
 
 //API方法求非空格字符串长度
 int getcount(char* str, int* pCount)
@@ -23,10 +24,34 @@ void main01()
 	getcount(p, &num);
 	cout << "count:" << num;
 }
+//去除字符串前后空格
+int trimSpace(char* str, char* newstr)
+{
+	char* p = str;
+	int ncount = 0;
+	if (str == NULL || newstr == NULL)
+		return -1;
+	int i = 0, j = 0;
+	j = strlen(p) - 1;
+	while (isspace(p[i]) && p[i] != '\0') i++;
+	while (isspace(p[j]) && p[j] != '\0') j--;
+	ncount = j - i + 1;
+	strncpy(newstr, str + i, ncount);
+	newstr[ncount] = '\0';
+	return 0;
+}
+void main02()
+{
+	char p[] = "    abcdefg       ";
+	char buf[1024] = { 0 };
+	int num = 0;
+	trimSpace(p, buf);
+	cout << "buf: " << buf << endl;
+}
 
-
-void main()//运行程序输出
+int main()//运行程序输出
 {
 	cout << "helloworld" << endl;
-	main01();
+	main02();
+	return 0;
 }
