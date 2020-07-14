96f1483f195447cd410aa1d5f8730f5815509000
子健 刘 <alphatk@vip.qq.com>
2020-07-14 22:15:32 +0800

    API方法求非空格字符串长度 main01();

--- 【C++提高02】两头堵模型/02两头堵模型.cpp
+++ 【C++提高02】两头堵模型/02两头堵模型.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 using namespace std;
-void main()
+
+//API方法求非空格字符串长度
+int getcount(char* str, int* pCount)
 {
-	cout << "helloworld";
+	char* p = str;
+	int ncount = 0;
+	if (str == NULL || pCount == NULL)
+		return -1;
+	int i = 0, j = 0;
+	j = strlen(p) - 1;
+	while (isspace(p[i]) && p[i] != '\0') i++;
+	while (isspace(p[j]) && p[j] != '\0') j--;
+	ncount = j - i + 1;
+	*pCount = ncount;
+	return 0;
+}
+void main01()
+{
+	char p[] = "    abcdefg    ";
+	int num = 0;
+	getcount(p, &num);
+	cout << "count:" << num;
+}
+
+
+void main()//运行程序输出
+{
+	cout << "helloworld" << endl;
+	main01();
 }
