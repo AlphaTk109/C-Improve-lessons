45ddee73bef7419504790020cb7481cfbebb0b7e
子健 刘 <alphatk@vip.qq.com>
2020-07-19 15:53:30 +0800

    指针做输入-排序示例

--- 【C++顺序结构提高04】二级指针及多级指针模型/二级指针输入模型-1.cpp
+++ 【C++顺序结构提高04】二级指针及多级指针模型/二级指针输入模型-1.cpp
@@ -1,6 +1,64 @@
 #include <iostream>
 using namespace std;
+#pragma warning (disable:4996)
+
+int main01()
+{
+	int i = 0;
+	char** p2 = NULL;
+	char* tmp = NULL;
+	int num = 5;
+	p2 = (char **)malloc(sizeof(char*) * num);
+	for (i = 0;i < num;i++)
+	{
+		p2[i] = (char*)malloc(sizeof(char) * 100);//char bu[100]
+		sprintf(p2[i], "%d%d%d", i + 1, i + 1, i + 1);
+	}
+	cout << "before:" << endl;
+	//ÅÅÐòÇ°´òÓ¡
+	for (i = 0;i < num;i++)
+	{
+		cout << p2[i] << endl;
+	}
+	//ÅÅÐò
+	for (i = 0;i < num;i++) 
+	{
+		for (int j = i + 1;j < num;j++) 
+		{
+			if (strcmp(p2[i], p2[j]) < 0)
+			{
+				tmp = p2[i];
+				p2[i] = p2[j];
+				p2[j] = tmp;
+			}
+		}
+	}
+
+	cout << "after:" << endl;
+	//ÅÅÐòºó´òÓ¡
+	for (i = 0;i < num;i++)
+	{
+		cout << p2[i]<<endl;
+	}
+
+
+	//ÊÍ·ÅÄÚ´æ
+	for(i=0;i<num;i++)
+	{
+		if (p2[i] != NULL)
+		{
+			free(p2[i]);
+			p2[i] = NULL;
+		}
+	}
+	if (p2 != NULL)
+	{
+		free(p2);
+	}
+	return 0;
+}
 int main()
 {
-	cout << "helloworld!test1";
+	main01();
+	return 1;
 }
