0d1239464bfd84bf4ed0d84f488e4e56bc32ef49
子健 刘 <alphatk@vip.qq.com>
2020-07-22 15:41:15 +0800

    数组及多维数组

--- 【C++顺序结构提高05】数组类型、本质及概念提高/数组提高01.cpp
+++ 【C++顺序结构提高05】数组类型、本质及概念提高/数组提高01.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 using namespace std;
-int main()
+int main51()
 {
-	cout << "helloworld";
+	//定义一个数组数据类型（注：int[5]与int不兼容）
+	typedef int(myint5)[5];//等价于int[5]
+	int i = 0;
+	myint5 array;//array[5]
+	for (i = 0;i < 5;i++) *(array + i) = i;
+	for (i = 0;i < 5;i++) cout<<*(array + i)<<endl;
+	cout << array<<endl;
+	cout << (array + 1)<<endl;
+	cout << &array << endl;
+	cout << (&array + 1) << endl;
+	return 0;
+}
+int main52()
+{
+	typedef int(*parray)[5];//通过数组定义数组指针：
+	parray array;//二维
+	int c[5] = { 0 };
+	array = &c;
+	for (int i = 0;i < 5;i++) **(array + i) = i;
+	for (int i = 0;i < 5;i++) cout << **(array + i) << endl;
+	return 0;
+}
+
+//定义数组指针变量的第三种方法：
+int main53()
+{
+	int(*parray)[5];
+	int c[5];
+	int i = 0;
+	parray = &c;
+	for (i = 0;i < 5;i++) (*parray)[i] = i + 1;
+	for (i = 0;i < 5;i++) cout << (*parray)[i] << endl;
+	return 0;
 }
---
+++ 【C++顺序结构提高05】数组类型、本质及概念提高/数组提高02-多维数组（本质及应用）.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	//多维度数组本质：
+	//(a+i)	 代表第i行的地址 二级指针
+	//*(a+i) 代表 一级指针 第i行元素的地址
+	//*(a+i)+j &a[i][j];
+}
